44c753903ab628c2f15fccd4808695e7b3800334
Frankie Gavin <frankanthonygavin@gmail.com>
2017-01-27 10:18:02 -0800

    use Firebase to configure, init, and connect my app's database.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { masterFirebaseConfig } from './api-keys';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { routing } from './app.routing';
 import { AboutComponent } from './about/about.component';
 import { BandComponent } from './band/band.component';
 import { MemberDetailComponent } from './member-detail/member-detail.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -22,9 +30,10 @@ import { MemberDetailComponent } from './member-detail/member-detail.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    routing
-
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
